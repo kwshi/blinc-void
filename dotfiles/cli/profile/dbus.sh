f3409a865a9455d6d2e89fb9822615a9c28d6c97
Kye Shi <shi.kye@gmail.com>
2021-02-14 14:08:53 -0800

    add dbus env profile script

---
+++ dotfiles/cli/profile/dbus.sh
@@ -0,0 +1,13 @@
+if [ ! -r '/var/lib/dbus/machine-id' ]; then return; fi
+read -r DBUS_MACHINE_ID < '/var/lib/dbus/machine-id'
+
+if [ -z "${HOME-}" -o -z "${DISPLAY-}" ]; then return; fi
+DBUS_SESSION_ENV="$HOME/.dbus/session-bus/$DBUS_MACHINE_ID-${DISPLAY#:}"
+
+. "$DBUS_SESSION_ENV"
+export \
+  DBUS_MACHINE_ID \
+  DBUS_SESSION_ENV \
+  DBUS_SESSION_BUS_ADDRESS \
+  DBUS_SESSION_BUS_PID \
+  DBUS_SESSION_BUS_WINDOWID
