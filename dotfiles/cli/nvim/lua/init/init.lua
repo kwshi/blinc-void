668a0d62b7a2fd1650b06c51be6044f4d4487b3e
Kye Shi <shi.kye@gmail.com>
2021-03-13 22:05:21 -0800

    some elm setup

--- dotfiles/cli/nvim/lua/init/init.lua
+++ dotfiles/cli/nvim/lua/init/init.lua
@@ -1,4 +1,7 @@
 local on_attach = function(opts)
+  opts.fmt = opts.fmt or true
+  opts.incr = opts.incr or false
+
   return function(client, buf)
     local set_key = function(k, v) 
       vim.api.nvim_buf_set_keymap(buf, 'n', k, v, {
@@ -7,18 +10,29 @@ local on_attach = function(opts)
       })
     end
     set_key('K', '<Cmd>lua vim.lsp.buf.hover()<CR>') 
-    if opts.fmt == nil or opts.fmt then
+    if opts.fmt then
       vim.api.nvim_exec('au BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)', false)
     end
+
+    if client.config.flags then
+      if opts.incr then
+        client.config.flags.allow_incremental_sync = true
+      end
+    end
+
   end
 end
 
 require 'lspconfig'.tsserver.setup {
-  on_attach = on_attach {fmt=false}
+  on_attach = on_attach { fmt = false }
 }
 
 require 'lspconfig'.svelte.setup {
   on_attach = on_attach {}
+}
+
+require 'lspconfig'.elmls.setup {
+  on_attach = on_attach { incr = true }
 }
 
 
