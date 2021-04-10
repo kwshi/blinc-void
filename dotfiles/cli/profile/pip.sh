c84dd892394d8e1ad80ff261d5218b772a5bc8e9
Kye Shi <shi.kye@gmail.com>
2021-04-09 18:52:48 -0700

    don't do the pip thing for now

--- dotfiles/cli/profile/pip.sh
+++ dotfiles/cli/profile/pip.sh
@@ -1,2 +1,2 @@
-export PYTHONPATH=/opt/blinc/pip/lib/python3.9/site-packages
-export PATH="/opt/blinc/pip/bin:$PATH"
+#export PYTHONPATH=/opt/blinc/pip/lib/python3.9/site-packages
+#export PATH="/opt/blinc/pip/bin:$PATH"
